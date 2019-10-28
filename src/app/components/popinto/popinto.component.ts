0ea24876c1486f394ae91b48a2dae739ba6f46e6
Guillermo <guillermo.echaguea@gmail.com>
2019-10-28 08:58:55 -0300

    Inicio del proyecto IONIC

---
+++ src/app/components/popinto/popinto.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popinto',
+  templateUrl: './popinto.component.html',
+  styleUrls: ['./popinto.component.scss'],
+})
+export class PopintoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
